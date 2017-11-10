eef9ca13827556c42a142cde5f48949966c6d5b6
Kun <thanhnt@greenglobal.vn>
2017-11-10 10:00:00 +0700

    Change config auth firebase

--- src/libraries/Fire.js
+++ src/libraries/Fire.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyBxahwUBrv0zBnGH5ni1E5ZI0MJuCezsvM",
-  authDomain: "luxe-dev-88038.firebaseapp.com",
-  databaseURL: "https://luxe-dev-88038.firebaseio.com/",
+  apiKey: "AIzaSyD7IEK2mKR4ERHemXAIVgzhP-DSNEgbJGQ",
+  authDomain: "luxeios-ca1f4.firebaseapp.com",
+  databaseURL: "https://luxeios-ca1f4.firebaseio.com/",
 };
 
 var fire = firebase.initializeApp(config);
